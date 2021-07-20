ced26c3f4790794f07a29816be0f584d6f79f593
Samantha Laine <samanthalaine8@gmail.com>
2021-07-20 10:56:29 -0400

    commit tree 251ec55d9626c290d376799e7021a0cc65daf5ea

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(){
+    return function inner(trait = '*'){
+    return `You are ${trait}!`
+    }
+}
+
+wrapAdjective('||a dedicated programmer!||')
